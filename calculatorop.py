2e341396f45b185b1d9da463bd48412c6bab6be5
Gene014 <calmaeugene06@gmail.com>
2023-05-22 02:20:56 +0800

    calculator operations done

--- calculatorop.py
+++ calculatorop.py
@@ -1,5 +1,14 @@
 #Calculator Operations
+class CalculatorOp:
 # input code for addition
+    def add (self, firstNum, secondNum):
+        return firstNum + secondNum
 # input code for subtraction
+    def subtract (self,firstNum, secondNum):
+        return firstNum - secondNum
 # input code for multiplication
+    def multiply (self, firstNum, secondNum):
+        return firstNum * secondNum
 # input code for division
+    def divide (self, firstNum, secondNum):
+        return firstNum / secondNum
