bbf2bd21638bd4d8d32273ce2563d376d8295cdd
Gene014 <calmaeugene06@gmail.com>
2023-05-22 19:45:44 +0800

    try again method and trycatch error indicated

--- mainprogram.py
+++ mainprogram.py
@@ -18,3 +18,14 @@ while True:
         "\n\33[1mPress \33[32m1\33[0m\33[1m if you want to \33[32mADD\33[0m\33[1m the variables. Press \33[32m2\33[0m\33[1m to \33[32mSUBTRACT\33[0m\33[1m. Press \33[32m3\33[0m\33[1m to \33[32mMULTIPLY\33[0m\33[1m. Or press \33[32m4\33[0m\33[1m to \33[32mDIVIDE\33[0m\33[1m. ")
     
     ui.performOperation(chosen_operation,username)
+    
+
+    # try again
+    askyesno = input("\n\33[46mDo you still want to continue? (yes/no):\33[0m ")
+    if askyesno.lower() == 'no':
+        print("\33[41mTerminating Program... ")
+        exit()
+    elif askyesno.lower() == 'yes':
+        continue
+    else:
+        raise TypeError
--- userint.py
+++ userint.py
@@ -21,19 +21,26 @@ class UserInt:
 
     def performOperation(self, chosen_Operation, username):
         calculate = CalculatorOp()
-
-        if chosen_Operation == "1":
-            summ = calculate.add(self.firstNumber, self.secondNumber)
-            print(f"Hey {username}, the sum is {summ}")
-        elif chosen_Operation == "2":
-            difference = calculate.subtract(
-                self.firstNumber, self.secondNumber)
-            print(f"Hey {username}, the difference is {difference}")
-        elif chosen_Operation == "3":
-            product = calculate.multiply(self.firstNumber, self.secondNumber)
-            print(f"Hey {username}, the product is {product}")
-        elif chosen_Operation == "4":
-            quotient = calculate.divide(self.firstNumber, self.secondNumber)
-            print(f"Hey {username}, the quotient is {quotient}")
-        else:
-            raise ValueError        
+        while True:
+            try:
+                if chosen_Operation == "1":
+                    summ = calculate.add(self.firstNumber, self.secondNumber)
+                    print(f"Hey {username}, the sum is {summ}")
+                elif chosen_Operation == "2":
+                    difference = calculate.subtract(
+                        self.firstNumber, self.secondNumber)
+                    print(f"Hey {username}, the difference is {difference}")
+                elif chosen_Operation == "3":
+                    product = calculate.multiply(self.firstNumber, self.secondNumber)
+                    print(f"Hey {username}, the product is {product}")
+                elif chosen_Operation == "4":
+                    quotient = calculate.divide(self.firstNumber, self.secondNumber)
+                    print(f"Hey {username}, the quotient is {quotient}")
+                else:
+                    raise ValueError
+            except TypeError:
+                print("Please check your typings to avoid errors! ")
+            except ValueError:
+                print("Numbers Only or Check your typings to avoid errors! ")
+            except ZeroDivisionError:
+                print("Undefined! Please Try Again!")        
